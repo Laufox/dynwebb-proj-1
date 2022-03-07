c215d7fda0aed1301e09d84c61b2fa16051046b9
Tim Nilsson <tim.r.nilsson@gmail.com>
2022-03-07 13:28:50 +0100

    Body data sent to /register must now be strings

--- validation/user_validation.js
+++ validation/user_validation.js
@@ -6,7 +6,7 @@ const models = require('../models');
 // Validation rules for creating a new user
 const registerRules = [
     // Email must be provided, must be a valid email and must not already exist in the database
-    body('email').exists().isEmail().custom(
+    body('email').exists().isString().isEmail().custom(
         // Custom validation function to check if the user email is already in the database
         async (email) => {
             // Try to get a new user from the databse where the email field equals what comes trough the request body
@@ -22,11 +22,11 @@ const registerRules = [
         }
     ),
     // Password must be provided and be minimum 6 charachters
-    body('password').exists().isLength({ min: 6 }),
+    body('password').exists().isString().isLength({ min: 6 }),
     // First name must be provided and be minimum 3 charachters
-    body('first_name').exists().isLength({ min: 3 }),
+    body('first_name').exists().isString().isLength({ min: 3 }),
     // Last name must be provided and be minimum 3 charachters
-    body('last_name').exists().isLength({ min: 3 })
+    body('last_name').exists().isString().isLength({ min: 3 })
 ];
 
 // Export the registerRules array
