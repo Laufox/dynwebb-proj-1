945f3b4738e9886995de527cbf957ce19803ce92
Tim Nilsson <tim.r.nilsson@gmail.com>
2022-04-02 14:08:03 +0200

    Changed route from /:albumId/photo to /:albumId/photos

--- routes/albums_route.js
+++ routes/albums_route.js
@@ -27,7 +27,7 @@ router.put('/:albumId', albumValidation.updateRules, albumsController.update);
  *  Direct POST /:albumId/photo traffic to the addPhoto method in albumsController file,
  *  using addPhotoRules method in albumsValidation file as validation middleware
  */
-router.post('/:albumId/photo', albumValidation.addPhotoRules, albumsController.addPhoto);
+router.post('/:albumId/photos', albumValidation.addPhotoRules, albumsController.addPhoto);
 
 // Export router for use in other files
 module.exports = router
